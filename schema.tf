8d2cd0c55ff2c9b3aa6324ba94789d85a56988dd
MuneerBSK <muneerbsk96@gmail.com>
2024-05-11 12:26:13 -0400

    AutoPush

--- schema.tf
+++ schema.tf
@@ -1,8 +1,6 @@
 # This is going to inject the MySQL Schema
 resource "null_resource" "schema" {
 
-       depends_on = [aws_db_instance.mysql]
-
   provisioner "local-exec" {
         command = <<EOF
         cd /tmp 
